f95d4529c3d27c4f2432ed53bdb2bfdf27b101d5
Dmitriy Shmatkov <chmatikd36@gmail.com>
2021-03-07 20:32:03 +0200

    Lab_2 (Activities)

--- Lab1/src/main/java/ua/opu/androidlabs/MainActivity.java
+++ Lab1/src/main/java/ua/opu/androidlabs/MainActivity.java
@@ -1,12 +1,11 @@
 package ua.opu.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
